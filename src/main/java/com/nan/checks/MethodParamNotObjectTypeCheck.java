42244d94381d4cadd175ffab2bb81249c2e15245
nkdalmia <nkdalmia@users.noreply.github.com>
2015-10-23 23:04:59 -0400

    method param not of Object type check

--- src/main/java/com/nan/checks/MethodParamNotObjectTypeCheck.java
+++ src/main/java/com/nan/checks/MethodParamNotObjectTypeCheck.java
@@ -5,11 +5,7 @@ import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 import com.puppycrawl.tools.checkstyle.checks.CheckUtils;
 
-/**
- * Created by ndalmia on 10/23/15.
- */
-public class MethodParamNoObjectCheck extends Check {
-
+public class MethodParamNotObjectTypeCheck extends Check {
 
     public int[] getDefaultTokens() {
         return new int[]{TokenTypes.METHOD_DEF};
@@ -22,20 +18,16 @@ public class MethodParamNoObjectCheck extends Check {
             return;
         }
 
-        // we can now be sure that there is at least one parameter
         final DetailAST parameters = ast.findFirstToken(TokenTypes.PARAMETERS);
         DetailAST child = parameters.getFirstChild();
         while (child != null) {
             // children are PARAMETER_DEF and COMMA
             if (child.getType() == TokenTypes.PARAMETER_DEF) {
                 final DetailAST paramType = child.findFirstToken(TokenTypes.TYPE);
-                final DetailAST firstNode = CheckUtils.getFirstNode(paramType);
-                log(firstNode.getLineNo(), firstNode.getColumnNo(), "Parameter type", paramType.getText());
-                /*if (paramType.getText())
-
-                    log(firstNode.getLineNo(), firstNode.getColumnNo(),
-                            MSG_KEY, paramName.getText());
-                }*/
+                if ("Object".equals(paramType.getFirstChild().getText())) {
+                    final DetailAST firstNode = CheckUtils.getFirstNode(child);
+                    log(firstNode.getLineNo(), firstNode.getColumnNo(), "Method parameter cannot be of type Object. Use Generic instead.");
+                }
             }
             child = child.getNextSibling();
         }
